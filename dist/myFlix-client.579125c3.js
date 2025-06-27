b5cb646e077ef70d58f3701fe3a741a495149400
Royjysmael <royjysmael@gmail.com>
2025-06-27 00:12:24 -0700

    Set up React app using Parcel with JSX and SCSS support
    - Created index.jsx with RoyFlix welcome component
    - Linked SCSS styles via index.scss
    - Configured index.js to render MyFlixApplication
    - Parcel bundling and live reload verified

---
+++ dist/myFlix-client.579125c3.js
@@ -0,0 +1,4 @@
+const root = document.getElementById("root");
+root.innerHTML = "<h1>Hello from RoyFlix!</h1>";
+
+//# sourceMappingURL=myFlix-client.579125c3.js.map
---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import "./styles/index.scss";
+
+const MyFlixApplication = () => {
+  return (
+    <div className="my-flix">
+      <h1>Welcome to RoyFlix 🎬</h1>
+    </div>
+  );
+};
+
+export default MyFlixApplication;
